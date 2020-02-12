554763901390b3cba8f536ce0a7d83a4cb5e2026
Daniel Bell <dchristianbell@gmail.com>
2020-02-12 15:02:59 -0700

    cleanup

--- src/cas/shorthand.clj
+++
@@ -1,8 +0,0 @@
-(ns cas.shorthand)
-
-;macros for convenience
-(defmacro pull-in [libname fnsyms]
-  `(do
-     ~@(for [fnsym fnsyms]
-         (do
-           `(def ~fnsym (~(symbol (str ".-" fnsym)) ~libname))))))
