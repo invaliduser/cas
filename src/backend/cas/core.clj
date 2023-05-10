2b46376c898272008405a101a2f1cf7516c01aee
Daniel Bell <dchristianbell@gmail.com>
2023-05-10 02:21:09 -0500

    ayyy we have a server

---
+++ src/backend/cas/core.clj
@@ -0,0 +1,25 @@
+(ns cas.core
+  (:require
+   [ring.adapter.jetty :as jetty]
+   [ring.middleware.file :refer [wrap-file]]
+   #_[org.httpkit.server :as h]))
+
+(def index-html (slurp "resources/public/index.html"))
+(def server (atom nil))
+
+(defn handler [req]
+  {:status  200
+   :body index-html})
+
+#_(defn start! []
+  (h/run-server handle {:port 3001}))
+(defn wrapped []
+  (-> handler
+      (wrap-file "resources/public")))
+
+(defn start! []
+  (reset! server (jetty/run-jetty (wrapped) {:port 3001
+                                      :join? false})))
+
+(defn stop! []
+  (.stop @server))
